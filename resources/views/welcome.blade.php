c7335d82fe154e5a10aecafb69d06f8142b9f3c1
liyijiehaha <2632379213@qq.com>
2019-04-25 11:56:31 +0000

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,8 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravelfdfdfd
+                    
                 </div>
 
                 <div class="links">
